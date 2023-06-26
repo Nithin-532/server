210cbedc0d0db464915a024d98808135e7db1871
Nithin Kalluru <70070522+Nithin-532@users.noreply.github.com>
2023-06-26 12:22:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.status(200).send("Welcome to root URL of Server");
 })
 
-app.listen(PORT, (error) => {
+app.listen(port, (error) => {
   if (!error) {
     console.log(`Server is Successfully Running, and App is listening on port ${PORT}`)
   } else {
